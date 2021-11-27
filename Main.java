fe4690fabcd6978de29f1227d751e00a83e85fff
Hernan Velasquez <velasquezmhernan@gmail.com>
2021-11-26 23:39:03 -0500

    Reconstruccio del proyecto de java con github

--- Main.java
+++
@@ -1,9 +0,0 @@
-import Persona;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-
-        Persona juan = new Persona("Juan");
-    }
-}
--- clases/Persona.java
+++
@@ -1,9 +0,0 @@
-public class Persona {
-    // private int id;
-    // private String name;
-    // private int age;
-    
-    public Persona( String name) {
-        System.out.println( name );
-    }
-}
