4f900f1ae63dbac270516350cc98b92f7fef9886
Hernan Velasquez <velasquezmhernan@gmail.com>
2021-11-26 23:06:05 -0500

    Carpeta de Clases

---
+++ Main.java
@@ -0,0 +1,9 @@
+import Persona;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+
+        Persona juan = new Persona("Juan");
+    }
+}
---
+++ clases/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    // private int id;
+    // private String name;
+    // private int age;
+    
+    public Persona( String name) {
+        System.out.println( name );
+    }
+}
