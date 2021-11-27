95c75c668b7e47138428736d3813864a7235da4c
Hernan Velasquez <velasquezmhernan@gmail.com>
2021-11-26 23:40:05 -0500

    Enpaqietando todo

---
+++ PracticaJava/src/App.java
@@ -0,0 +1,11 @@
+import clases.Persona;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        
+        Persona juan = new Persona("Juan");
+
+        juan.nombre();
+    }
+}
---
+++ PracticaJava/src/clases/Persona.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class Persona {
+    private String name;
+    
+    public Persona(String name) {
+        this.name = name;
+    }
+
+    public void nombre() {
+        System.out.println(this.name);
+    }
+}
