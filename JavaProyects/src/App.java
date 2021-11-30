23f613215afbf5e301a574b99e56f94835d1c53f
Hernan Velasquez <velasquezmhernan@gmail.com>
2021-11-30 13:14:46 -0500

    Restructurando el proyecto de practica

--- JavaProyects/src/App.java
+++ JavaProyects/src/App.java
@@ -2,10 +2,6 @@ import clases.Persona;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        
-        Persona juan = new Persona("Hernan Carechimba");
-
-        juan.nombre();
+    
     }
 }
